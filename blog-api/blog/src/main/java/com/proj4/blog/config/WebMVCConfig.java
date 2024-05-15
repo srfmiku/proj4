060c4ce46635f711ced7b036174b0879d9f22a69
srf <848918988@qq.com>
2024-05-15 10:48:16 +0800

    v2

--- blog-api/blog/src/main/java/com/proj4/blog/config/WebMVCConfig.java
+++ blog-api/blog/src/main/java/com/proj4/blog/config/WebMVCConfig.java
@@ -1,13 +1,20 @@
 package com.proj4.blog.config;
 
+import com.proj4.blog.common.JacksonObjectMapper;
 import com.proj4.blog.handler.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@Slf4j
 public class WebMVCConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
@@ -25,5 +32,18 @@ public class WebMVCConfig implements WebMvcConfigurer {
                 .addPathPatterns("/test")
                 .addPathPatterns("/comments/create/change")
                 .addPathPatterns("/articles/publish");
+    }
+    /**
+     * 拓展mvc框架的消息转换器
+     * @param converters
+     */
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        log.info("拓展消息转换器");
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转化为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器容器
+        converters.add(0,messageConverter);
     }
 }
